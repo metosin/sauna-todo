aa2e752653676e9537e4cf664439336f8d2065ed
Matti Nieminen <matti.nieminen@metosin.fi>
2017-04-04 19:48:32 +0300

    Fix bug where empty todo list caused problem when adding new ones

--- src/clj/backend/todo.clj
+++ src/clj/backend/todo.clj
@@ -13,9 +13,10 @@
 
 (defn add-todo!
   [message]
-  (let [next-id (inc (apply max (map :id @todos)))
+  (let [existing-ids (map :id @todos)
+        new-id (inc (apply max 0 (conj existing-ids)))
         {:keys [send! send-fn]} message]
-    (swap! todos conj {:id next-id
+    (swap! todos conj {:id new-id
                        :text (:body message)})
     (broadcast/broadcast! {:message-type :todos
                            :body @todos})))
