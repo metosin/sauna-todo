d1c10226947449ba784294087d2a464b30a1a6af
Matti Nieminen <matti.nieminen@metosin.fi>
2017-04-04 10:08:54 +0300

    Update basic information about the project in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sauna-todo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple full-stack Clojure TODO app example for TiTe and Luuppi -sauna"
+  :url "https://github.com/metosin/sauna-todo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
