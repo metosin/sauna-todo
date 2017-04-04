66e1177143eefb327bc0cdbe2027373c47090568
Matti Nieminen <matti.nieminen@metosin.fi>
2017-04-04 13:13:36 +0300

    Add integrant-repl to project and implement reloaded workflow

--- project.clj
+++ project.clj
@@ -5,9 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [integrant "0.3.3"]
+                 [integrant/repl "0.2.0"]
                  [org.immutant/web "2.1.6"]]
   :main ^:skip-aot backend.main
   :source-paths ["src/clj" "src/cljc"]
   :test-paths ["test/clj" "test/cljc"]
   :target-path "target/%s"
+  :repl-options {:init-ns user}
   :profiles {:uberjar {:aot :all}})
---
+++ src/clj/user.clj
@@ -0,0 +1,7 @@
+(ns user
+  (:require [integrant.core :as ig]
+            [integrant.repl :refer [clear go halt init reset reset-all]]
+            [backend.server]))
+
+(integrant.repl/set-prep! (fn []
+                            (ig/read-string (slurp "resources/config.edn"))))
