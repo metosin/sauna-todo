dddd4b6fc211631574dea186f1cfdced0f9bbf46
Matti Nieminen <matti.nieminen@metosin.fi>
2017-04-04 17:28:36 +0300

    Add function for broadcasting with eines

---
+++ src/clj/backend/broadcast.clj
@@ -0,0 +1,9 @@
+(ns backend.broadcast
+  (:require [eines.core :as eines]))
+
+(defn broadcast!
+  [message]
+  (let [sockets (vals @eines/sockets)]
+    (doseq [socket sockets]
+      (let [{:keys [send!]} socket]
+        (send! message)))))
