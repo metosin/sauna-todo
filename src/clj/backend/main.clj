f2f5ea08fc96a5291fdce438d1e2b20ec206e54c
Juho Teperi <juho.teperi@iki.fi>
2017-10-09 13:49:28 +0300

    Fix reading config file from uberjar

--- src/clj/backend/main.clj
+++ src/clj/backend/main.clj
@@ -1,8 +1,9 @@
 (ns backend.main
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
 (defn -main [& args]
   (require 'backend.server)
   (let [config ((resolve 'integrant.core/read-string)
-                (slurp "resources/config.edn"))]
+                (slurp (io/resource "config.edn")))]
     ((resolve 'integrant.core/init) config)))
