5567751d7d5b4ff835259b413c5d91cd86aafca8
Tommi Reiman <tommi@iki.fi>
2017-11-02 15:27:54 +0200

    this todo

--- src/clj/intro/demo.clj
+++ src/clj/intro/demo.clj
@@ -1,8 +1,24 @@
 (ns intro.demo)
 
+;; basics?
+
+;; play with the app
+
+;; get remote data
+
 ;; User:
 ;;  - name
 ;;  - gender
 ;;  - location
 ;;    - street
 ;;    - city
+
+;; push data to frontend
+
+;; rest api
+
+;; testing
+
+;; browser testing
+
+
