983bafa0be6c377eb2c1ca53eadcb02e13bfb829
Tommi Reiman <tommi@iki.fi>
2017-12-19 10:01:08 +0200

    Clojure 1.9.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/metosin/sauna-todo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [binaryage/devtools "0.9.7"]
                  [integrant "0.6.1"]
